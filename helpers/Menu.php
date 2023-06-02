1fdc239366910d5940685381c6a20a66fa639c64
sangayulan <135309466+irfannugraha17@users.noreply.github.com>
2023-06-02 14:13:38 +0700

    First commit

---
+++ helpers/Menu.php
@@ -0,0 +1,113 @@
+<?php
+/**
+ * Menu Items
+ * All Project Menu
+ * @category  Menu List
+ */
+
+class Menu{
+	
+	
+			public static $navbartopleft = array(
+		array(
+			'path' => 'home', 
+			'label' => 'Home', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'commodities', 
+			'label' => 'Commodities', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'commodity_locations', 
+			'label' => 'Commodity Locations', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'failed_jobs', 
+			'label' => 'Failed Jobs', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'migrations', 
+			'label' => 'Migrations', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'password_resets', 
+			'label' => 'Password Resets', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'school_operational_assistances', 
+			'label' => 'School Operational Assistances', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'users', 
+			'label' => 'Users', 
+			'icon' => ''
+		)
+	);
+		
+			public static $navbarsideleft = array(
+		array(
+			'path' => 'home', 
+			'label' => 'Home', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'commodities', 
+			'label' => 'Commodities', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'commodity_locations', 
+			'label' => 'Commodity Locations', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'failed_jobs', 
+			'label' => 'Failed Jobs', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'migrations', 
+			'label' => 'Migrations', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'password_resets', 
+			'label' => 'Password Resets', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'school_operational_assistances', 
+			'label' => 'School Operational Assistances', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'users', 
+			'label' => 'Users', 
+			'icon' => ''
+		)
+	);
+		
+	
+	
+}
